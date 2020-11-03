dfca58d35457a0c564e753899517dcd5e2bb5de8
ttzztztz <ttzztztz@outlook.com>
2020-11-03 10:07:03 +0800

    1103

--- 广度优先搜索/1036. Escape a Large Maze.cpp
+++ 广度优先搜索/1036. Escape a Large Maze.cpp
@@ -1,69 +1,83 @@
 class Solution {
 public:
-    int N = 0, M = 0;
-    bool pointValid(int x, int y) {
-        return x >= 0 && y >= 0 && x <= N && y <= M;
-    }
     bool isEscapePossible(vector<vector<int>>& blocked, vector<int>& source, vector<int>& target) {
-        vector<int> allX, allY;
-        for (const vector<int>& block : blocked) {
-            allX.push_back(block[0]), allY.push_back(block[1]);
-            if (block[0] - 1 >= 0) allX.push_back(block[0] - 1);
-            // if (block[1] - 1 >= 0) allY.push_back(block[1] - 1);
-            
-            // if (block[0] + 1 < 1e6) allX.push_back(block[0] + 1);
-            if (block[1] + 1 < 1e6) allY.push_back(block[1] + 1);
+        prepare(blocked, source, target);
+
+        auto pointValid = [&](int i, int j) -> bool {
+            return i >= 0 && j >= 0 && i < n && j < m;
+        };
+        vector<vector<bool>> visited(n + 1, vector<bool>(m + 1, false));
+        typedef pair<int, int> State;
+        
+        deque<State> q = { {source[0], source[1]} };
+        visited[source[0]][source[1]] = true;
+        State finalState = { target[0], target[1] };
+        
+        for (auto& v : blocked) {
+            visited[v[0]][v[1]] = true;
         }
-        allX.push_back(source[0]), allX.push_back(target[0]);
-        allY.push_back(source[1]), allY.push_back(target[1]);
         
-        sort(allX.begin(), allX.end()), sort(allY.begin(), allY.end());
-        auto endX = unique(allX.begin(), allX.end());
-        auto endY = unique(allY.begin(), allY.end());
-        
-        for (vector<int>& block : blocked) {
-            block[0] = lower_bound(allX.begin(), endX, block[0]) - allX.begin();
-            block[1] = lower_bound(allY.begin(), endY, block[1]) - allY.begin();
+        const int dx[] = {0, 0, -1, 1};
+        const int dy[] = {-1, 1, 0, 0};
+
+        while (!q.empty()) {
+            auto state = q.front();
+            auto [x, y] = state;
+            q.pop_front();
+            visited[x][y] = true;
             
-            N = max(N, block[0]), M = max(M, block[1]);
-        }
-        source[0] = lower_bound(allX.begin(), endX, source[0]) - allX.begin();
-        source[1] = lower_bound(allY.begin(), endY, source[1]) - allY.begin();
-        target[0] = lower_bound(allX.begin(), endX, target[0]) - allX.begin();
-        target[1] = lower_bound(allY.begin(), endY, target[1]) - allY.begin();
-        N = max(N, source[0]), M = max(M, source[1]);
-        N = max(N, target[0]), M = max(M, target[1]);
-        
-        set<pair<int, int>> visited;
-        visited.emplace(source[0], source[1]);
-        
-        deque<pair<int, int>> queue;
-        queue.emplace_back(source[0], source[1]);
-        
-        const int dx[] = {0, 0, -1, 1}, dy[] = {-1, 1, 0, 0};
-        while (!queue.empty()) {
-            int x, y;
-            tie(x, y) = queue.front();
-            queue.pop_front();
-            
-            if (x == target[0] && y == target[1]) {
-                return true;
-            }
-            
+            if (state == finalState) return true;
             for (int k = 0; k < 4; k++) {
-                const int nextX = x + dx[k], nextY = y + dy[k];
+                const int nx = x + dx[k], ny = y + dy[k];
+                if (make_pair(nx, ny) == finalState) return true;
+                if (!pointValid(nx, ny) || visited[nx][ny]) continue;
                 
-                if (pointValid(nextX, nextY) && !visited.count({nextX, nextY}) && find(blocked.begin(), blocked.end(), vector<int>{nextX, nextY}) == blocked.end()) {
-                    visited.emplace(nextX, nextY);
-                    queue.emplace_back(nextX, nextY);
-                }
-                
-                if (nextX == target[0] && nextY == target[1]) {
-                    return true;
-                }
+                q.emplace_back(nx, ny);
+                visited[nx][ny] = true;
             }
         }
         
         return false;
+    }
+private:
+    int n = 0, m = 0;
+    void prepare(vector<vector<int>>& blocked, vector<int>& source, vector<int>& target) {
+        constexpr auto valid = [&](int i) -> bool {
+            const int fin = 1e6;
+            return i >= 0 && i < fin;
+        };
+
+        vector<int> allx, ally;
+        for (auto& v : blocked) {
+            for (int k = -1; k <= 1; k++) {
+                if (valid(v[0] + k)) allx.push_back(v[0] + k);
+                if (valid(v[1] + k)) ally.push_back(v[1] + k);
+            }
+        }
+        
+        for (int k = -1; k <= 1; k++) {
+            if (valid(source[0] + k)) allx.push_back(source[0] + k);
+            if (valid(source[1] + k)) ally.push_back(source[1] + k);
+            
+            if (valid(target[0] + k)) allx.push_back(target[0] + k);
+            if (valid(target[1] + k)) ally.push_back(target[1] + k);
+        }
+        
+        sort(allx.begin(), allx.end());
+        sort(ally.begin(), ally.end());
+        auto endx = unique(allx.begin(), allx.end());
+        auto endy = unique(ally.begin(), ally.end());
+        
+        source[0] = lower_bound(allx.begin(), endx, source[0]) - allx.begin();
+        source[1] = lower_bound(ally.begin(), endy, source[1]) - ally.begin();
+        target[0] = lower_bound(allx.begin(), endx, target[0]) - allx.begin();
+        target[1] = lower_bound(ally.begin(), endy, target[1]) - ally.begin();
+        
+        for (auto& v : blocked) {
+            v[0] = lower_bound(allx.begin(), endx, v[0]) - allx.begin();
+            v[1] = lower_bound(ally.begin(), endy, v[1]) - ally.begin();
+        }
+        
+        n = endx - allx.begin(), m = endy - ally.begin();
     }
 };
