1fcd065b6d35e2e8fba118204e0e2cfb1528e997
ttzztztz <ttzztztz@outlook.com>
2021-02-13 16:17:33 +0800

    0213

--- 广度优先搜索/1091. Shortest Path in Binary Matrix.cpp
+++ 广度优先搜索/1091. Shortest Path in Binary Matrix.cpp
@@ -1,23 +1,38 @@
-#include <iostream>
-#include <cstring>
-#include <algorithm>
-#include <vector>
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        const int n = grid.size();
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        typedef pair<int, int> PII;
+        
+        if (grid[0][0] == 1) return -1;
+        deque<PII> q = { {0, 0} };
+        
+        const int dx[] = {0, 0, -1, 1, -1, -1, 1, 1};
+        const int dy[] = {-1, 1, 0, 0, -1, 1, -1, 1};
+        
+        auto point_valid = [&](int x, int y) -> bool {
+            return x >= 0 && y >= 0 && x < n && y < n;
+        };
 
-using namespace std;
-
-int main () {
-    vector<int> v = {1,1,1,1,1,2,2,2,2,2,2,3,4,5,6};
-    int left = 0, right = v.size() - 1;
-
-    const int val = 2;
-    while (left < right) {
-        const int middle = (left + right) >> 1;
-        if (val < v[middle]) {
-            right = middle;
-        } else {
-            left = middle + 1;
+        int len = 1;
+        while (!q.empty()) {
+            const int cnt = q.size();
+            for (int _ = 0; _ < cnt; _++) {
+                auto [x, y] = q.front();
+                q.pop_front();
+                if (x == n - 1 && y == n - 1) return len;
+                visited[x][y] = true;
+                
+                for (int k = 0; k < 8; k++) {
+                    const int nx = x + dx[k], ny = y + dy[k];
+                    if (!point_valid(nx, ny) || visited[nx][ny] || grid[nx][ny] == 1) continue;
+                    q.emplace_back(nx, ny);
+                    visited[nx][ny] = true;
+                }
+            }
+            len++;
         }
+        return -1;
     }
-
-    cout << left << endl << right;
-}
+};
