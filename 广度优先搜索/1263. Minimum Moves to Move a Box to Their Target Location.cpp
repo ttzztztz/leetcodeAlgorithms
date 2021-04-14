105d552fcfb31e9f5329aaa34d2467371403cdad
ttzztztz <ttzztztz@outlook.com>
2021-04-14 09:23:22 +0800

    0414

--- 广度优先搜索/1263. Minimum Moves to Move a Box to Their Target Location.cpp
+++ 广度优先搜索/1263. Minimum Moves to Move a Box to Their Target Location.cpp
@@ -1,118 +1,86 @@
-class Point {
-public:
-    int x, y;
-
-    Point() : x(0), y(0) {};
-
-    Point(int a, int b) : x(a), y(b) {};
-
-    bool operator==(const Point &p) const {
-        return this->x == p.x && this->y == p.y;
-    }
-};
+const int dx[] = {0, 0, -1, 1};
+const int dy[] = {-1, 1, 0, 0};
 
 class Solution {
 public:
-    int N, M;
-
-    bool canReach(const Point &start, const Point &end, const vector<vector<char>> &grid, const Point &box) {
-        if (start == end) {
-            return true;
+    int minPushBox(vector<vector<char>>& grid) {
+        if (grid.empty()) return -1;
+        n = grid.size(), m = grid[0].size();
+        this->g = grid;
+        
+        for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) {
+            if (grid[i][j] == 'S') s = {i, j};
+            else if (grid[i][j] == 'B') b = {i, j};
+            else if (grid[i][j] == 'T') e = {i, j};
         }
 
-        deque<Point> current;
-        vector<vector<bool>> visited(N + 3, vector<bool>(M + 3, false));
-        visited[start.x][start.y] = true;
+        bool visited[n + 1][m + 1][4];
+        memset(visited, 0, sizeof visited);
 
-        current.push_back(start);
-        while (!current.empty()) {
-            Point p = current.front();
-            current.pop_front();
-
-            if (p == end) {
-                return true;
-            }
-
-            const int dx[] = {0, 0, -1, 1};
-            const int dy[] = {-1, 1, 0, 0};
-
-            for (int i = 0; i < 4; i++) {
-                const int nextX = p.x + dx[i], nextY = p.y + dy[i];
-                if (!pointValid(Point(nextX, nextY)) || visited[nextX][nextY] || grid[nextX][nextY] == '#' || box == Point(nextX, nextY)) {
-                    continue;
-                }
-                visited[nextX][nextY] = true;
-                current.emplace_back(nextX, nextY);
-            }
+        int d = 0;
+        deque<State> q;
+        for (int k = 0; k < 4; k++) {
+            Point end = { b.first + dx[k], b.second + dy[k] };
+            if (can_reach(s, b, end)) q.emplace_back(b.first + dx[k], b.second + dy[k], k);
         }
 
-        return false;
-    }
+        while (!q.empty()) {
+            const int cnt = q.size();
+            for (int _ = 0; _ < cnt; _++) {
+                auto [x, y, k] = q.front();
+                visited[x][y][k] = true;
+                q.pop_front();
 
-    bool pointValid(const Point &p) {
-        return p.x >= 0 && p.y >= 0 && p.x < N && p.y < M;
-    }
-
-    Point nextBox(Point current, int dir) {
-        const int dx[] = {1, 0, 0, -1};
-        const int dy[] = {0, 1, -1, 0};
-
-        current.x += dx[dir], current.y += dy[dir];
-        return current;
-    }
-
-    int minPushBox(vector<vector<char>> &grid) {
-        Point start, target, box;
-        N = grid.size(), M = grid[0].size();
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (grid[i][j] == 'S') {
-                    grid[i][j] = '.';
-                    start.x = i, start.y = j;
-                } else if (grid[i][j] == 'B') {
-                    grid[i][j] = '.';
-                    box.x = i, box.y = j;
-                } else if (grid[i][j] == 'T') {
-                    grid[i][j] = '.';
-                    target.x = i, target.y = j;
-                }
-            }
-        }
-
-        deque<tuple<Point, Point, int>> queue;
-        set<tuple<int, int, int, int>> stateVisted;
-
-        queue.emplace_back(start, box, 0);
-        stateVisted.insert({start.x, start.y, box.x, box.y});
-        while (!queue.empty()) {
-            const tuple<Point, Point, int> t = queue.front();
-            Point stateStart = get<0>(t), stateBox = get<1>(t);
-            int step = get<2>(t);
-
-            queue.pop_front();
-            if (stateBox == target) {
-                return step;
-            }
-            const int dx[] = {-1, 0, 0, 1};
-            const int dy[] = {0, -1, 1, 0};
-
-            for (int i = 0; i < 4; i++) {
-                const Point adjacentBox = Point(stateBox.x + dx[i], stateBox.y + dy[i]);
-                if (pointValid(adjacentBox) && canReach(adjacentBox, stateStart, grid, stateBox) && grid[adjacentBox.x][adjacentBox.y] == '.') {
-
-                    Point next_box = nextBox(stateBox, i);
-                    if (pointValid(next_box) && grid[next_box.x][next_box.y] == '.') {
-                        if (!stateVisted.count({stateBox.x, stateBox.y, next_box.x, next_box.y})) {
-                            stateVisted.insert({stateBox.x, stateBox.y, next_box.x, next_box.y});
-                            queue.emplace_back(stateBox, next_box, step + 1);
+                if (make_pair(x, y) == e) return d;
+                const int nx = x + (-1) * dx[k], ny = y + (-1) * dy[k];
+                if (point_valid(nx, ny) && g[nx][ny] != '#') {
+                    for (int nk = 0; nk < 4; nk++) {
+                        if (
+                            can_reach({x, y}, {nx, ny}, {nx + dx[nk], ny + dy[nk]})
+                            && !visited[nx][ny][nk]
+                        ) {
+                            visited[nx][ny][nk] = true;
+                            if (make_pair(nx, ny) == e) return d;
+                            q.emplace_back(nx, ny, nk);
                         }
                     }
-
                 }
             }
-
+            d++;
         }
         return -1;
+    }
+private:
+    typedef pair<int, int> Point;
+    typedef tuple<int, int, int> State;
+    int n, m;
+    Point s, e, b;
+    vector<vector<char>> g;
+    
+    bool point_valid(int i, int j) {
+        return i >= 0 && j >= 0 && i < n && j < m;
+    }
+    
+    bool can_reach(const Point& start, const Point& box, const Point& end) {
+        if (!point_valid(start.first, start.second) || !point_valid(end.first, end.second) || g[start.first][start.second] == '#' || g[end.first][end.second] == '#') return false;
+
+        deque<Point> q = {start};
+        bool visited[n + 1][m + 1];
+        memset(visited, 0, sizeof visited);
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop_front();
+            visited[x][y] = true;
+            if (make_pair(x, y) == end) return true;
+            
+            for (int k = 0; k < 4; k++) {
+                const int nx = x + dx[k], ny = y + dy[k];
+                if (!point_valid(nx, ny) || visited[nx][ny] || g[nx][ny] == '#' || make_pair(nx, ny) == box) continue;
+                visited[nx][ny] = true;
+                if (make_pair(nx, ny) == end) return true;
+                q.emplace_back(nx, ny);
+            }
+        }
+        return false;
     }
 };
