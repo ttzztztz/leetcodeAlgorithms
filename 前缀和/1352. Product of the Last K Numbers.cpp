21b2f9d813a6d019e86349ee64169cff21c31954
ttzztztz <ttzztztz@outlook.com>
2021-05-05 10:51:23 +0800

    0505

--- 前缀和/1352. Product of the Last K Numbers.cpp
+++ 前缀和/1352. Product of the Last K Numbers.cpp
@@ -1,32 +1,27 @@
 class ProductOfNumbers {
 public:
-    vector<long long> data;
-    vector<long long> prefix;
-    set<int> zeroPos;
     ProductOfNumbers() {
-        prefix.push_back(1);
+        
     }
     
     void add(int num) {
         if (num == 0) {
-            zeroPos.insert(prefix.size());
-            data.push_back(num);
-            prefix.push_back(1);
+            last_zero = n;
+            pref.push_back(1);
         } else {
-            prefix.push_back(num * (*prefix.rbegin()));
+            pref.push_back(pref.back() * num);
         }
+        n++;
     }
     
     int getProduct(int k) {
-        const int N = prefix.size();
-        const int last = N - 1, begin = N - k - 1;
-        auto it = zeroPos.lower_bound(begin + 1);
-        if (it != zeroPos.end()) {
-            return 0;
-        } else {
-            return prefix[last] / prefix[begin];
-        }
+        const int l = n - k;
+        if (l <= last_zero) return 0;
+        return pref.back() / pref[l];
     }
+private:
+    vector<int> pref = {1};
+    int last_zero = -1, n = 0;
 };
 
 /**
