45084a0f5e3a479c5978065917c713253587e84d
ttzztztz <ttzztztz@outlook.com>
2021-05-11 22:25:17 +0800

    0511

--- 字符串/KMP/1392. Longest Happy Prefix.cpp
+++ 字符串/KMP/1392. Longest Happy Prefix.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     string longestPrefix(string s) {
-        const int N = s.size();
-        vector<int> next(N + 2, 0);
-        next[0] = -1, next[1] = 0;
-        
+        const int n = s.size();
+        vector<int> next(n + 1, -1);
+        next[1] = 0;
         int i = 1, j = 0;
-        while (i <= N) {
+        while (i < n) {
             if (j == -1 || s[i] == s[j]) {
                 next[++i] = ++j;
             } else {
                 j = next[j];
             }
         }
-        
-        return s.substr(0, next[N]);
+        return s.substr(0, next[n]);
     }
 };
