fd0b6a7de59648c67abc98e4728dce9254acbb9c
ttzztztz <ttzztztz@outlook.com>
2021-05-14 20:43:17 +0800

    0514

--- 博弈论/1406. Stone Game III.cpp
+++ 博弈论/1406. Stone Game III.cpp
@@ -1,39 +1,22 @@
 class Solution {
 public:
     string stoneGameIII(vector<int>& stoneValue) {
-        for (int i : stoneValue) sum += i;
+        const int n = stoneValue.size(), sum = accumulate(stoneValue.begin(), stoneValue.end(), 0);
+        vector<vector<int>> f(n + 1, vector<int>(2, 0));
         
-        const int N = stoneValue.size();
-        f = vector<vector<int>>(N + 5, vector<int>(2, -1));
-        
-        int answer = dfs(stoneValue, 0, 0);
-        
-        if (answer > sum - answer) return "Alice";
-        else if (answer < sum - answer) return "Bob";
-        else return "Tie";
-    }
-private:
-    int sum = 0;
-    vector<vector<int>> f;
-    int dfs(const vector<int>& stoneValue, int i, int j) {
-        if (i == stoneValue.size()) return 0;
-        if (f[i][j] != -1) return f[i][j];
-        
-        int answer;
-        if (j == 0) { // Alice turn
-            answer = -99999999;
-            int sum = 0;
-            for (int k = 0; k < 3 && i + k < stoneValue.size(); k++) {
-                sum += stoneValue[i + k];
-                answer = max(answer, sum + dfs(stoneValue, i + k + 1, j ^ 1));
-            }
-        } else { // Bob turn
-            answer = 99999999;
-            for (int k = 0; k < 3 && i + k < stoneValue.size(); k++) {
-                answer = min(answer, dfs(stoneValue, i + k + 1, j ^ 1));
+        for (int i = n - 1; i >= 0; i--) {
+            int s = 0;
+            
+            f[i][0] = numeric_limits<int>::min(), f[i][1] = numeric_limits<int>::max();
+            for (int k = 1; k <= 3 && i + k <= n; k++) {
+                s += stoneValue[i + k - 1];
+                f[i][0] = max(f[i][0], f[i + k][1] + s);
+                f[i][1] = min(f[i][1], f[i + k][0]); // error
             }
         }
         
-        return f[i][j] = answer;
+        if (f[0][0] > sum - f[0][0]) return "Alice";
+        else if (f[0][0] < sum - f[0][0]) return "Bob";
+        else return "Tie";
     }
 };
