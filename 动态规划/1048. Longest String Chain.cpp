e4cbdc7f723091ccf5233e98a1f29d8e6c616c97
ttzztztz <ttzztztz@outlook.com>
2021-05-17 19:46:49 +0800

    0517

--- 动态规划/1048. Longest String Chain.cpp
+++ 动态规划/1048. Longest String Chain.cpp
@@ -1,52 +1,37 @@
 class Solution {
 public:
-    bool judge(const string& sub, const string& super) {
-        if (sub.size() + 1 == super.size()) {
-            bool used = false;
-            int ptr1 = 0, ptr2 = 0;
-            while (ptr2 < super.size()) {
-                if (ptr1 < sub.size() && sub[ptr1] == super[ptr2]) {
-                    ptr1++, ptr2++;
-                } else {
-                    if (used) {
-                        return false;
-                    }
-
-                    ptr2++;
-                    used = true;
+    int longestStrChain(vector<string>& words) {
+        const int n = words.size();
+        int ans = 0;
+        sort(words.begin(), words.end(), [&](const auto& lhs, const auto& rhs) -> bool {
+            return lhs.size() < rhs.size();
+        });
+        
+        vector<int> f(n, 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (ok(words[j], words[i])) f[i] = max(f[i], f[j] + 1);
+            }
+            ans = max(ans, f[i]);
+        }
+        return ans;
+    }
+private:
+    bool ok(const string& from, const string& to) {
+        if (from.size() + 1 == to.size()) {
+            for (int i = 0, j = 0, changed = 0; i < to.size();) {
+                if (from[j] == to[i]) {
+                    i++, j++;
+                    continue;
+                } else { // from[i] != to[j]
+                    if (changed) return false;
+                    else changed = true;
+                    i++;
                 }
             }
-
             return true;
         } else {
             return false;
         }
-    }
-
-    int longestStrChain(vector<string>& words) {
-        sort(words.begin(), words.end(), [](const string& $1, const string& $2)->bool{
-            return $1.size() < $2.size();
-        });
-        const int N = words.size();
-        vector<int> dp(N + 5, 1);
-        int answer = 1;
-
-        for (int i = 0; i < N; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (words[j].size() + 1 > words[i].size()) {
-                    continue;
-                } else if (words[j].size() + 1 < words[i].size()) {
-                    break;
-                }
-
-                if (judge(words[j], words[i])) {
-                    dp[i] = max(dp[i], dp[j] + 1);
-                }
-
-                answer = max(answer, dp[i]);
-            }
-        }
-
-        return answer;
     }
 };
--- 动态规划/968. Binary Tree Cameras.cpp
+++ 动态规划/968. Binary Tree Cameras.cpp
@@ -4,25 +4,60 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int minCameraCover(TreeNode* root) {
-        tuple<int, int, int> answer = dp(root);
-        return min(get<1>(answer), get<2>(answer));
+        if (root == nullptr) return 0;
+        memset(f, 0x3f, sizeof f);
+        f[0][0] = 0;
+        f[0][1] = f[0][2] = 0x3f3f3f3f;
+        
+        dfs(root);
+        // for (int i = 1; i <= 5; i++) {
+        //     cout << f[i][0] << " " << f[i][1] << " " << f[i][2] << endl;
+        // }
+        return min(f[1][0], f[1][2]);
     }
-    tuple<int, int, int> dp(TreeNode* root) {
-        if (root == nullptr) {
-            return make_tuple(0, 0, 999999);
+private:
+    unordered_map<TreeNode*, int> id;
+    int f[1005][3];
+    // 0 -> place light
+    // 1 -> not place, from parent
+    // 2 -> not place, from child
+    int cnt = 0;
+    
+    void dfs(TreeNode* root) {
+        if (root == nullptr) return;
+        id[root] = ++cnt;
+        
+        if (root->left) dfs(root->left);
+        if (root->right) dfs(root->right);
+        
+        const int u = id[root], l = id[root->left], r = id[root->right];
+        if (!root->left && !root->right) {
+            f[u][0] = 1;
+            f[u][1] = 0;
+            f[u][2] = 1005;
+            return;
         }
-        tuple<int, int, int> left = dp(root->left), right = dp(root->right);
-
-        int state0 = get<1>(left) + get<1>(right);
-        int state1 = min(get<2>(left) + min(get<1>(right), get<2>(right)), get<2>(right) + min(get<1>(left), get<2>(left)));
-        int state2 = 1 + min(get<0>(left), min(get<1>(left), get<2>(left))) + min(get<0>(right), min(get<1>(right), get<2>(right)));
-
-        return make_tuple(state0, state1, state2);
+        
+        for (int i = 0; i <= 2; i++) for (int j = 0; j <= 2; j++) {
+            f[u][0] = min(f[u][0],
+                          f[l][i] + f[r][j] + 1);
+        }
+        
+        f[u][1] = min(
+            min(f[l][0] + f[r][0], f[l][2] + f[r][2]),
+            min(f[l][0] + f[r][2], f[l][2] + f[r][0])
+        );
+        
+        f[u][2] = f[l][0] + f[r][0];
+        if (root->left) f[u][2] = min(f[u][2], f[l][0] + f[r][2]);
+        if (root->right) f[u][2] = min(f[u][2], f[l][2] + f[r][0]);
     }
 };
