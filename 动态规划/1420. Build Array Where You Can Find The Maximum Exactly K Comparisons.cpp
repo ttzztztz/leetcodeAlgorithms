6030adb16bc552fccc41aaea58ea45d6ad269d46
ttzztztz <ttzztztz@outlook.com>
2021-05-20 20:42:06 +0800

    0520

--- 动态规划/1420. Build Array Where You Can Find The Maximum Exactly K Comparisons.cpp
+++ 动态规划/1420. Build Array Where You Can Find The Maximum Exactly K Comparisons.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
     int numOfArrays(int n, int m, int k) {
-        N = n, M = m, K = k;
-        memset(f, 0xff, sizeof f);
-        return dfs(0, 0, 0);
-    }
-private:
-    const int MOD = 1e9+7;
-    long long f[55][105][55];
-    int N, M, K;
-    long long dfs(int n, int m, int k) {
-        if (k > K) return 0;
-        if (N == n) {
-            return k == K;
-        }
-        if (f[n][m][k] != -1) return f[n][m][k];
+        typedef long long ll;
+        const int mod = 1e9+7;
+        vector<vector<ll>> f(m + 1, vector<ll>(k + 2, 0));
+        for (int i = 1; i <= m; i++) f[i][k] = 1;
         
-        long long answer = 0;
-        if (m != 0) {
-            for (int p = 1; p <= m; p++) {
-                answer += dfs(n + 1, m, k);
-                answer %= MOD;
+        for (int i = n - 1; i >= 0; i--) {
+            vector<vector<ll>> g(m + 1, vector<ll>(k + 2, 0));
+            for (int j = 0; j <= m; j++) for (int x = 0; x <= k; x++) {
+                
+                for (int y = 1; y <= m; y++) {
+                    g[j][x] = (g[j][x] + f[max(y, j)][y > j ? (x + 1) : x]) % mod;
+                }
+                
             }
+            f = g;
         }
         
-        for (int p = m + 1; p <= M; p++) {
-            answer += dfs(n + 1, p, k + 1);
-            answer %= MOD;
-        }
-        
-        return f[n][m][k] = answer;
+        return f[0][0];
     }
 };
--- 广度优先搜索/102. Binary Tree Level Order Traversal.cpp
+++ 广度优先搜索/102. Binary Tree Level Order Traversal.cpp
@@ -4,38 +4,31 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> answer;
-        if (root == nullptr) {
-            return answer;
-        }
-
-        deque<pair<TreeNode*, int>> queue;
-        queue.emplace_back(root, 0);
-        int lastLayer = -1;
-        while (!queue.empty()) {
-            pair<TreeNode*, int> item = queue.front();
-            queue.pop_front();
-
-            if (lastLayer != item.second) {
-                lastLayer = item.second;
-                answer.push_back(vector<int>());
-            }
-
-            answer[item.second].push_back(item.first->val);
-
-            if (item.first->left != nullptr) {
-                queue.emplace_back(item.first->left, item.second + 1);
-            }
-            if (item.first->right != nullptr) {
-                queue.emplace_back(item.first->right, item.second + 1);
+        vector<vector<int>> ans;
+        if (root == nullptr) return ans;
+        
+        deque<TreeNode*> q = {root};
+        while (!q.empty()) {
+            ans.push_back(vector<int>{});
+            const int cnt = q.size();
+            for (int _ = 0; _ < cnt; _++) {
+                const auto u = q.front();
+                q.pop_front();
+                
+                ans.back().push_back(u->val);
+                
+                if (u->left) q.push_back(u->left);
+                if (u->right) q.push_back(u->right);
             }
         }
-        return answer;
+        return ans;
     }
 };
