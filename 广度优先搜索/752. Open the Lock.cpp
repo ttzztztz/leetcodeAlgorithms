be3b32a9b2098abdc4edc0fbe13321dd3b588870
ttzztztz <ttzztztz@outlook.com>
2021-06-05 14:55:05 +0800

    0605

--- 广度优先搜索/752. Open the Lock.cpp
+++ 广度优先搜索/752. Open the Lock.cpp
@@ -1,57 +1,42 @@
 class Solution {
 public:
-    string nextState(string prev, int digit) {
-        int d = prev[digit] - '0';
-        d++;
-        if (d >= 10) d -= 10;
-        prev[digit] = d + '0';
-        return prev;
-    }
-    string prevState(string prev, int digit) {
-        int d = prev[digit] - '0';
-        d--;
-        if (d < 0) d += 10;
-        prev[digit] = d + '0';
-        return prev;
-    }
-
     int openLock(vector<string>& deadends, string target) {
-        unordered_set<string> deadendSet;
-        for (const string& s : deadends) {
-            deadendSet.insert(s);
-        }
-
-        if (deadendSet.count("0000") || deadendSet.count(target)) return -1;
-
-        unordered_set<string> visited;
-        deque<pair<string, int>> queue;
-        queue.emplace_back("0000", 0);
-        visited.insert("0000");
-
-        while (!queue.empty()) {
-            string state;
-            int dist;
-            tie(state, dist) = queue.front();
-            queue.pop_front();
-
-            if (state == target) {
-                return dist;
-            }
-
-            for (int i = 0; i < 4; i++) {
-                string next = nextState(state, i);
-                if (!deadendSet.count(next) && !visited.count(next)) {
-                    visited.insert(next);
-                    queue.emplace_back(next, dist + 1);
+        unordered_set<string> visited(deadends.begin(), deadends.end());
+        if (visited.count(target) || visited.count("0000")) return -1;
+        
+        deque<string> q = { "0000" };
+        int ans = 0;
+        while (!q.empty()) {
+            const int cnt = q.size();
+            for (int _ = 0; _ < cnt; _++) {
+                const string u = q.front();
+                q.pop_front();
+                if (u == target) return ans;
+                
+                for (int x = 0; x < 4; x++) {
+                    const int d = u[x] - '0';
+                    const int prev = (d - 1 + 10) % 10;
+                    const int next = (d + 1) % 10;
+                    
+                    string a1 = u;
+                    a1[x] = '0' + prev;
+                    if (!visited.count(a1)) {
+                        visited.insert(a1);
+                        if (a1 == target) return ans + 1;
+                        q.push_back(a1);
+                    }
+                    
+                    string a2 = u;
+                    a2[x] = '0' + next;
+                    if (!visited.count(a2)) {
+                        visited.insert(a2);
+                        if (a2 == target) return ans + 1;
+                        q.push_back(a2);
+                    }
                 }
-                string prev = prevState(state, i);
-                if (!deadendSet.count(prev) && !visited.count(prev)) {
-                    visited.insert(prev);
-                    queue.emplace_back(prev, dist + 1);
-                }
             }
+            ans++;
         }
-
         return -1;
     }
 };
