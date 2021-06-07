3ad1c962e3ba683e2dbe05074b95c29ae12aec45
ttzztztz <ttzztztz@outlook.com>
2021-06-07 19:36:36 +0800

    0607

--- 前缀与后缀/1888. Minimum Number of Flips to Make the Binary String Alternating.cpp
+++ 前缀与后缀/1888. Minimum Number of Flips to Make the Binary String Alternating.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
     int minFlips(string s) {
-        this->s = s, n = s.size();
+        const int n = s.size();
+        
+        int f[100005][2];
+        memset(f, 0, sizeof f);
+        f[0][0] = (s[0] != '0'), f[0][1] = (s[0] != '1');
+        for (int i = 1; i < n; i++) {
+            f[i][0] = f[i - 1][1] + (s[i] != '0');
+            f[i][1] = f[i - 1][0] + (s[i] != '1');
+        }
+        
+        auto query = [&](int r, int r_last) -> int {
+            if (r < 0) return 0;
+            return f[r][r_last];
+        };
         int ans = n;
-        
-        memset(f, 0xff, sizeof f);
-        memset(g, 0xff, sizeof g);
-        
         for (int i = -1; i <= n; i++) {
             const int len1 = i + 1, len2 = n - i - 1;
             
-            const int a = dfs1(i, len1 % 2 ? 1 : 0) + dfs2(i + 1, len2 % 2 ? 0 : 1);
-            const int b = dfs1(i, len1 % 2 ? 0 : 1) + dfs2(i + 1, len2 % 2 ? 1 : 0);
-            
-            // cout << i << "," << dfs1(i, len1 % 2 ? 1 : 0) << "," << dfs2(i + 1, len2 % 2 ? 0 : 1) << endl;
-            // cout << dfs1(i, len1 % 2 ? 0 : 1) << "," << dfs2(i + 1, len2 % 2 ? 1 : 0) << endl;
+            const int a = query(n - 1, 0) - query(i, (len2 % 2 == 0) ? 0 : 1) + query(i, (len1 % 2 == 1) ? 1 : 0);
+            const int b = query(n - 1, 1) - query(i, (len2 % 2 == 0) ? 1 : 0) + query(i, (len1 % 2 == 1) ? 0 : 1);
             ans = min(ans, min(a, b));
         }
         return ans;
-    }
-private:
-    int n;
-    int f[100005][2], g[100005][2];
-    string s;
-    
-    int dfs1(int idx, int cur) {
-        if (idx <= -1) return 0;
-        
-        int& val = f[idx][cur];
-        if (val != -1) return val;
-        
-        int ans = dfs1(idx - 1, cur ^ 1) + (s[idx] - '0' != cur);
-        return val = ans;
-    }
-    
-    int dfs2(int idx, int cur) {
-        if (idx >= n) return 0;
-        
-        int& val = g[idx][cur];
-        if (val != -1) return val;
-        
-        int ans = dfs2(idx + 1, cur ^ 1) + (s[idx] - '0' != cur);
-        return val = ans;
     }
 };
--- 哈希/128. Longest Consecutive Sequence.cpp
+++ 哈希/128. Longest Consecutive Sequence.cpp
@@ -1,29 +1,49 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if (0 == nums.size()) {
-            return 0;
-        }
-        int answer = 1;
-        unordered_set<int> set;
-        for (int num : nums) {
-            set.insert(num);
+        if (nums.empty()) return 0;
+        const int mi = *min_element(nums.begin(), nums.end());
+        if (mi < 0) {
+            for (int& i : nums) i += (-mi);
         }
 
-        for (int num : nums) {
-            int currentAnswer = 1;
-            if (set.count(num - 1)) {
-                continue;
+        bucket_sort(nums);
+        const int n = nums.size();
+        int ans = 1, cur = 1;
+        unordered_map<int, int> cnt;
+        for (int i = 0; i < n; i++) {
+            if (cnt.count(nums[i] - 1)) {
+                cnt[nums[i]] = cnt[nums[i] - 1] + 1;
+                ans = max(ans, cnt[nums[i]]);
+            } else {
+                cnt[nums[i]] = 1;
             }
-
-            while (set.count(num + 1)) {
-                num++;
-                currentAnswer++;
-            }
-
-            answer = std::max(answer, currentAnswer);
         }
-
-        return answer;
+        return ans;
+    }
+private:
+    typedef long long ll;
+    void bucket_sort(vector<int>& nums) {
+        const int n = nums.size();
+        const int max_nums = *max_element(nums.begin(), nums.end());
+        vector<int> buf(n);
+        ll exp = 1;
+        while (exp <= max_nums) {
+            vector<int> cnt(10, 0);
+            for (int i = 0; i < n; i++) {
+                const int d = (nums[i] / exp) % 10;
+                cnt[d]++;
+            }
+            
+            for (int i = 1; i < 10; i++) cnt[i] += cnt[i - 1];
+            
+            for (int i = n - 1; i >= 0; i--) {
+                const int d = (nums[i] / exp) % 10;
+                buf[cnt[d] - 1] = nums[i];
+                cnt[d]--;
+            }
+            exp *= 10;
+            nums = buf;
+        }
     }
 };
