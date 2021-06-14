19d1ba1eec81ca2e2ee8917b7d3a5781a9930a55
ttzztztz <ttzztztz@outlook.com>
2021-06-14 10:20:21 +0800

    0614

--- 哈希/336. Palindrome Pairs.cpp
+++ 哈希/336. Palindrome Pairs.cpp
@@ -1,60 +1,51 @@
+// special case ["a", ""]
+
 class Solution {
 public:
     vector<vector<int>> palindromePairs(vector<string>& words) {
         vector<vector<int>> ans;
-        unordered_map<string, vector<int>> h;
         
         const int n = words.size();
-        vector<vector<bool>> visited(n, vector<bool>(n, false));
-        for (int i = 0; i < n; i++) h[words[i]].push_back(i);
-        
-        for (int i = 0; i < n; i++) {
-            // case 1
-            string str = words[i];
-            reverse(str.begin(), str.end());
-            if (h.count(str)) {
-                for (int v : h[str]) {
-                    if (v != i && !visited[i][v]) ans.push_back(vector<int>{ i, v }), visited[i][v] = true;
+        unordered_map<string, int> all;
+        for (int j = 0; j < n; j++) all[words[j]] = j;
+        for (int j = 0; j < n; j++) {
+            const string& word = words[j];
+            const int m = word.size();
+            
+            string rev_word = word;
+            reverse(rev_word.begin(), rev_word.end());
+            if (all.count(rev_word) && j != all[rev_word]) {
+                ans.push_back({ j, all[rev_word] });
+            }
+            
+            string t;
+            for (int i = 0; i <= m - 1; i++) {
+                t += word[i];
+                string rev_right = word.substr(i + 1);
+                reverse(rev_right.begin(), rev_right.end());
+                
+                if (is_palindrome(t) && all.count(rev_right) && j != all[rev_right]) {
+                    ans.push_back({ all[rev_right], j });
                 }
             }
             
-            // case 2
-            for (int j = 0; j < words[i].size(); j++) {
-                if (isPalindrome(words[i], j, words[i].size() - 1)) {
-                    str.clear();
-                    for (int k = 0; k < j; k++) str += words[i][k];
-                    reverse(str.begin(), str.end());
-                    
-                    if (h.count(str)) {
-                        for (int v : h[str]) {
-                            if (v != i && !visited[i][v]) ans.push_back(vector<int>{ i, v }), visited[i][v] = true;
-                        }
-                    }
-                }
-            }
-            
-            // case 3
-            for (int j = 0; j < words[i].size(); j++) {
-                if (isPalindrome(words[i], 0, j)) {
-                    str.clear();
-                    for (int k = j + 1; k < words[i].size(); k++) str += words[i][k];
-                    reverse(str.begin(), str.end());
-                    
-                    if (h.count(str)) {
-                        for (int v : h[str]) {
-                            if (v != i && !visited[v][i]) ans.push_back(vector<int>{ v, i }), visited[v][i] = true;
-                        }
-                    }
+            t.clear();
+            for (int i = m - 1; i >= 0; i--) {
+                t = word[i] + t;
+                string rev_left = word.substr(0, i);
+                reverse(rev_left.begin(), rev_left.end());
+                
+                if (is_palindrome(t) && all.count(rev_left) && j != all[rev_left]) {
+                    ans.push_back({ j, all[rev_left] });
                 }
             }
         }
-        
         return ans;
     }
 private:
-    bool isPalindrome(const string& str, int i, int j) {
-        for (int a = i, b = j; a < b; a++, b--) {
-            if (str[a] != str[b]) return false;
+    bool is_palindrome(const string& str) {
+        for (int i = 0, j = str.size() - 1; i < j; i++, j--) {
+            if (str[i] != str[j]) return false;
         }
         return true;
     }
