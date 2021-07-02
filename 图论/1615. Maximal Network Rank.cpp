c38ecbd0f1abf774716010395404fc32520f45fb
ttzztztz <ttzztztz@outlook.com>
2021-07-02 19:30:15 +0800

    0702

--- 图论/1615. Maximal Network Rank.cpp
+++ 图论/1615. Maximal Network Rank.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<vector<int>> edges(n, vector<int>(n, 0));
-        for (auto& v : roads) {
-            edges[v[0]][v[1]] = edges[v[1]][v[0]] = 1;
+        deg = vector<int>(n, 0);
+        for (auto& road : roads) {
+            deg[road[0]]++, deg[road[1]]++;
+            connected.insert(hash(road[0], road[1]));
         }
         
-        bool visited[105][105];
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int ext = 0;
-                if (edges[i][j]) ext = 1;
-                memset(visited, 0, sizeof visited);
-                visited[i][j] = true;
-                
-                for (int k = 0; k < n; k++) {
-                    if (k == i || k == j) continue;
-                    if (!visited[i][k] && edges[i][k]) {
-                        visited[i][k] = true;
-                        ext++;
-                    }
-                    if (!visited[k][j] && edges[k][j]) {
-                        visited[k][j] = true;
-                        ext++;
-                    }
-                }
-                
-                ans = max(ans, ext);
-            }
+        for (int u = 0; u < n; u++) for (int v = u + 1; v < n; v++) {
+            int t = deg[u] + deg[v];
+            if (connected.count(hash(u, v))) t--;
+            ans = max(ans, t);
         }
         return ans;
+    }
+private:
+    vector<int> deg;
+    unordered_set<string> connected;
+    
+    string hash(int a, int b) {
+        if (a > b) swap(a, b);
+        return to_string(a) + "," + to_string(b);
     }
 };
--- 图论/1617. Count Subtrees With Max Distance Between Cities.cpp
+++ 图论/1617. Count Subtrees With Max Distance Between Cities.cpp
@@ -1,71 +1,59 @@
 class Solution {
 public:
     vector<int> countSubgraphsForEachDiameter(int n, vector<vector<int>>& edges) {
-        this->n = n;
-        const int INF = 99999;
-        w = vector<vector<int>>(n, vector<int>(n, INF));
-        ans = vector<int>(n - 1, 0);
-        visited = vector<bool>((1 << n) + 5, false);
+        const int inf = numeric_limits<int>::max() / 2;
         
-        for (auto& v : edges) w[v[0] - 1][v[1] - 1] = w[v[1] - 1][v[0] - 1] = 1;
-        for (int i = 0; i < n; i++) w[i][i] = 0;
+        this->n = n;
+        ans = vector<int>(n - 1, 0);
+        visited = vector<bool>((1 << n) + 1, false);
+        dist = vector<vector<int>>(n + 1, vector<int>(n + 1, inf));
+        
+        for (auto& e : edges) {
+            int &u = e[0], &v = e[1];
+            u--, v--;
+
+            dist[u][v] = dist[v][u] = 1;
+        }
+        for (int i = 0; i < n; i++) dist[i][i] = 0;
         
         for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    w[i][j] = min(w[i][j], w[i][k] + w[k][j]);
-                }
+            for (int u = 0; u < n; u++) for (int v = 0; v < n; v++) {
+                dist[u][v] = min(dist[u][v], dist[u][k] + dist[k][v]);
             }
         }
-        
-//         for (int i = 0; i < n; i++) {
-//             for (int j = 0; j < n; j++) {
-//                 cout << i << ", " << j << ", " << w[i][j] << endl;
-//             }
-//         }
-        
-        for (int u = 0; u < n; u++) {
-            for (int v = u + 1; v < n; v++) {
-                if (w[u][v] != 1) continue;
-                
-                if (!visited[(1 << u) | (1 << v)]) {
-                    visited[(1 << u) | (1 << v)] = true;
-                    dfs((1 << u) | (1 << v));
-                }
-            }
+
+        for (auto& e : edges) {
+            const int u = e[0], v = e[1];
+            dfs((1 << u) | (1 << v));
         }
         return ans;
     }
 private:
     vector<int> ans;
     vector<bool> visited;
-    vector<vector<int>> w;
+    vector<vector<int>> dist;
     int n;
     
     void dfs(int state) {
-        int dist = 1;
+        if (visited[state]) return;
+        visited[state] = true;
+        
+        int d = 1;
         for (int u = 0; u < n; u++) {
             if (!(state & (1 << u))) continue;
             for (int v = 0; v < n; v++) {
                 if (!(state & (1 << v))) continue;
-                
-                dist = max(dist, w[u][v]);
+                d = max(d, dist[u][v]);
             }
         }
-        ans[dist - 1]++;
+        ans[d - 1]++;
         
         for (int u = 0; u < n; u++) {
-            if (!(state & (1 << u))) continue;
-            for (int v = 0; v < n; v++) {
-                if (state & (1 << v)) continue;
-                if (w[u][v] != 1) continue;
-                
-                if (!visited[state | (1 << v)]) {
-                    visited[state | (1 << v)] = true;
-                    dfs(state | 1 << v);
+            if (state & (1 << u)) for (int v = 0; v < n; v++) {
+                if (!(state & (1 << v)) && dist[u][v] == 1) {
+                    dfs(state | (1 << v));
                 }
             }
         }
-        
     }
 };
