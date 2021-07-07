f74592dc1ece31e7bae4fb5023fd1d1563ecd4ba
ttzztztz <ttzztztz@outlook.com>
2021-07-07 20:26:07 +0800

    0707

--- 贪心/1338. Reduce Array Size to The Half.cpp
+++ 贪心/1338. Reduce Array Size to The Half.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     int minSetSize(vector<int>& arr) {
         const int n = arr.size();
-        unordered_map<int, int> a;
-        for (int i : arr) a[i]++;
+        unordered_map<int, int> appear;
+        for (int i : arr) appear[i]++;
         
-        vector<int> b;
-        for (auto& [k, v] : a) b.push_back(v);
-        sort(b.begin(), b.end());
+        vector<int> t;
+        for (auto& [k, v] : appear) t.push_back(v);
+        sort(t.begin(), t.end());
         
-        int ans = 0, cur = 0;
-        for (int i = b.size() - 1; i >= 0; i--) {
-            cur += b[i];
+        int removed = 0, ans = 0, idx = t.size() - 1;
+        const int should_remove = (n + 1) / 2;
+        while (removed < should_remove) {
+            removed += t[idx];
+            idx--;
             ans++;
-            if (cur >= n / 2) break;
         }
         return ans;
     }
