b50dd99708c09689b8eb60de6d1b85f73d684c11
ttzztztz <ttzztztz@outlook.com>
2021-07-15 20:12:58 +0800

    0715

--- 排序/791. Custom Sort String.cpp
+++ 排序/791. Custom Sort String.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    string customSortString(string S, string T) {
-        unordered_map<char, int> app;
-        for (int i = 0; i < S.size(); i++) app[S[i]] = i;
-        sort(T.begin(), T.end(), [&](const auto& $1, const auto& $2) -> bool {
-            return app[$1] < app[$2];
+    string customSortString(string order, string str) {
+        unordered_map<char, int> ord;
+        for (int i = 0; i < order.size(); i++) ord[order[i]] = i;
+        sort(str.begin(), str.end(), [&](const auto& lhs, const auto& rhs) -> bool {
+            return ord[lhs] < ord[rhs];
         });
-        return T;
+        return str;
     }
 };
