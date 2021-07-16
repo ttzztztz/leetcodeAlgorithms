ca8e2c035be6d4a2ecbb9c4e6fc73994ea05a151
ttzztztz <ttzztztz@outlook.com>
2021-07-16 20:40:11 +0800

    0716

--- 二分查找/611. Valid Triangle Number.cpp
+++ 二分查找/611. Valid Triangle Number.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
-		const int n = nums.size();
-		if (n <= 2) return 0;
-		sort(nums.begin(), nums.end());
-
-		int ans = 0;
-		for (int i = 0; i < n; i++) {
-			if (nums[i] == 0) continue;
-			for (int j = i + 1; j < n; j++) {
-				const int s = nums[i] + nums[j];
-				auto it = --lower_bound(nums.begin() + j, nums.end(), s);
-				ans += max(0, (int) (it - (nums.begin() + j)));
+        const int n = nums.size();
+        int ans = 0;
+        
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) continue;
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[j] == 0) continue;
+                
+                auto it = lower_bound(nums.begin(), nums.end(), nums[i] + nums[j]);
+                ans += it - (nums.begin() + j) - 1;
             }
         }
-		return ans;
+        return ans;
     }
 };
-
-
--- 多指针问题/611. Valid Triangle Number.cpp
+++ 多指针问题/611. Valid Triangle Number.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
-        const int n = nums.size();
-        if (n <= 2) return 0;
-
-        sort(nums.begin(), nums.end());
         int ans = 0;
+        
+        sort(nums.begin(), nums.end());
+        const int n = nums.size();
         for (int i = 0; i < n; i++) {
-            int k = i + 1;
-            for (int j = i + 1; j < n; j++) {
-                k = max(k, j + 1);
-                const int s = nums[i] + nums[j];
-                while (k < n && s > nums[k]) {
-                    k++;
-                }
-                ans += k - j - 1;
+            for (int j = i + 1, k = j + 1; j < n; j++) {
+                while (k < n && nums[i] + nums[j] > nums[k]) k++;
+                ans += max(0, k - j - 1);
             }
         }
-
+        
         return ans;
     }
 };
-
-
