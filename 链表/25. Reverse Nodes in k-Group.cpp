0820d681ee4962f7b871f2d9b6f4011c29c0ec68
ttzztztz <ttzztztz@outlook.com>
2021-07-19 20:04:36 +0800

    0719

--- 链表/25. Reverse Nodes in k-Group.cpp
+++ 链表/25. Reverse Nodes in k-Group.cpp
@@ -3,47 +3,53 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode* flag = new ListNode(0);
+        ListNode* flag = new ListNode();
         flag->next = head;
         
-        ListNode* cur = head, *prev = flag;
+        ListNode *prev = flag, *cur = head;
         while (cur) {
-            ListNode* probe = cur, *prob_prev = nullptr;
+            ListNode* prob = cur;
             
-            int i = 0;
-            for (; i < k; i++) {
-                if (probe == nullptr) break;
-                prob_prev = probe;
-                probe = probe->next;
+            bool ok = true;
+            for (int i = 0; i < k; i++) {
+                if (prob == nullptr) {
+                    ok = false;
+                    break;
+                }
+                prob = prob->next;
             }
             
-            if (i < k) {
-                prev->next = cur;
-                break;
-            }
+            if (!ok) break;
             
-            reverse(cur, probe);
-            prev->next = prob_prev;
+            ListNode* new_head = reverse(cur, prob);
+            prev->next = new_head;
+            cur->next = prob;
+            
             prev = cur;
-            cur = probe;
+            cur = prob;
         }
         
-        return flag->next;
+        ListNode* ans = flag->next;
+        delete flag;
+        return ans;
     }
 private:
-    void reverse(ListNode* start, ListNode* end) {
-        ListNode* cur = start, *prev = nullptr;
+    ListNode* reverse(ListNode* head, ListNode* end) {
+        ListNode* prev = nullptr, *cur = head;
         while (cur != end) {
-            ListNode* tmp = cur->next;
+            ListNode* t = cur->next;
             cur->next = prev;
             prev = cur;
-            cur = tmp;
+            cur = t;
         }
+        return prev;
     }
 };
