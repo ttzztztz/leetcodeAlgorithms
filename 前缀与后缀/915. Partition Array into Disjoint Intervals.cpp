d9ba48b7349229de7b06eec2420029e911025bf8
ttzztztz <ttzztztz@outlook.com>
2021-07-23 19:48:24 +0800

    0723

--- 前缀与后缀/915. Partition Array into Disjoint Intervals.cpp
+++ 前缀与后缀/915. Partition Array into Disjoint Intervals.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    int partitionDisjoint(vector<int>& A) {
-        const int n = A.size();
-        vector<int> f(n), g(n);
-        f[0] = A[0], g[n - 1] = A[n - 1];
-        for (int i = 1; i < n; i++) f[i] = max(f[i - 1], A[i]);
-        for (int i = n - 2; i >= 0; i --) g[i] = min(g[i + 1], A[i]);
+    int partitionDisjoint(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        const int n = nums.size();
+        
+        vector<int> l(n), r(n);
+        l[0] = nums[0];
+        for (int i = 1; i < n; i++) l[i] = max(l[i - 1], nums[i]);
+        
+        r[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) r[i] = min(r[i + 1], nums[i]);
         
         for (int i = 0; i + 1 < n; i++) {
-            if (f[i] <= g[i + 1]) return i + 1;
+            if (l[i] <= r[i + 1]) return i + 1;
         }
-        return n - 1;
+        return 0; // never reach this branch
     }
 };
