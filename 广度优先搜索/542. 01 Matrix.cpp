e44a6501453caa2126ed523aa8431e2380f036c0
Rabbit <ttzztztz@outlook.com>
2021-07-29 20:09:39 +0800

    0729

--- 广度优先搜索/542. 01 Matrix.cpp
+++ 广度优先搜索/542. 01 Matrix.cpp
@@ -1,51 +1,49 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
-        const int N = matrix.size(), M = matrix[0].size();
-        vector<vector<int>> answer(N, vector<int>(M, 9999999));
-        vector<vector<bool>> visited(N, vector<bool>(M, false));
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        if (mat.empty()) return {};
+        const int n = mat.size(), m = mat[0].size();
+        vector<vector<int>> ans(n, vector<int>(m, 0));
         
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
         typedef pair<int, int> PII;
         deque<PII> q;
         
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (matrix[i][j] == 0) {
-                    answer[i][j] = 0;
-                    q.emplace_back(i, j);
-                    visited[i][j] = true;
-                }
+        for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) {
+            if (mat[i][j] == 0) {
+                visited[i][j] = true;
+                q.emplace_back(i, j);
             }
         }
         
-        const int dx[] = {0, 0, -1, 1};
-        const int dy[] = {1, -1, 0, 0};
+        const int dx[] = { 0, 0, -1, 1 };
+        const int dy[] = { -1, 1, 0, 0 };
         
-        auto pointValid = [&](int i, int j) -> bool {
-            return i >= 0 && j >= 0 && i < N && j < M;
+        auto point_valid = [&](int i, int j) -> bool {
+            return i >= 0 && j >= 0 && i < n && j < m;
         };
-        
-        int d = 0;
+
+        int dist = 0;
         while (!q.empty()) {
             const int cnt = q.size();
             for (int _ = 0; _ < cnt; _++) {
-                int i, j;
-                tie(i, j) = q.front();
+                auto [x, y] = q.front();
                 q.pop_front();
                 
-                if (d < answer[i][j]) answer[i][j] = d;
+                ans[x][y] = dist;
+                visited[x][y] = true;
                 
                 for (int k = 0; k < 4; k++) {
-                    const int nx = i + dx[k], ny = j + dy[k];
-                    if (pointValid(nx, ny) && !visited[nx][ny]) {
+                    const int nx = x + dx[k], ny = y + dy[k];
+                    if (point_valid(nx, ny) && !visited[nx][ny]) {
                         visited[nx][ny] = true;
                         q.emplace_back(nx, ny);
                     }
                 }
             }
-            d++;
+            dist++;
         }
         
-        return answer;
+        return ans;
     }
 };
