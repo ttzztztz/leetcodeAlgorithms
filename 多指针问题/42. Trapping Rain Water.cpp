1f671d9f205bd2fbaba7af154f69493331ac5477
Rabbit <ttzztztz@outlook.com>
2021-07-31 17:56:26 +0800

    0731

--- 多指针问题/42. Trapping Rain Water.cpp
+++ 多指针问题/42. Trapping Rain Water.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        if (0 == height.size()) {
-            return 0;
-        }
-        int answer = 0;
-
-        int leftPtr = 0, rightPtr = height.size() - 1;
-        int leftMax = 0, rightMax = 0;
-        while (leftPtr < rightPtr) {
-            if (height[leftPtr] < height[rightPtr]) {
-                leftMax = std::max(leftMax, height[leftPtr]);
-                answer += leftMax - height[leftPtr];
-                leftPtr++;
+        if (height.empty()) return 0;
+        
+        int ans = 0;
+        const int n = height.size();
+        for (int l = 0, r = n - 1, lval = height[0], rval = height[n - 1]; l < r;) {
+            if (height[l] < height[r]) {
+                ans += max(0, lval - height[l]);
+                lval = max(lval, height[l]);
+                l++;
             } else {
-                rightMax = std::max(rightMax, height[rightPtr]);
-                answer += rightMax - height[rightPtr];
-                rightPtr--;
+                ans += max(0, rval - height[r]);
+                rval = max(rval, height[r]);
+                r--;
             }
         }
-
-        return answer;
+        
+        return ans;
     }
 };
