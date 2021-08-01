033ea16200d67b6e977deb774892fa85f2b8c0ad
ttzztztz <ttzztztz@outlook.com>
2021-08-01 19:22:00 +0800

    0801

--- 并查集/827. Making A Large Island.cpp
+++ 并查集/827. Making A Large Island.cpp
@@ -1,105 +1,75 @@
 class Solution {
 public:
-    int parent[2600], size[2600];
-    int N, M;
-
-    int ID(int i, int j) {
-        return i * M + j;
-    }
-    void init() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                parent[ID(i, j)] = ID(i, j);
-                size[ID(i, j)] = 1;
-            }
-        }
-    }
-    int findParent(int u) {
-        if (parent[u] == u) {
-            return u;
-        } else {
-            return parent[u] = findParent(parent[u]);
-        }
-    }
-    void merge(int u, int v) {
-        int parentU = findParent(u), parentV = findParent(v);
-        if (parentU != parentV) {
-            parent[parentU] = parentV;
-            size[parentV] += size[parentU];
-        }
-    }
-    bool pointValid(int i, int j) {
-        return i >= 0 && j >= 0 && i < N && j < M;
-    }
     int largestIsland(vector<vector<int>>& grid) {
-        N = grid.size(), M = grid[0].size();
+        if (grid.empty()) return 0;
+        n = grid.size(), m = grid[0].size();
+        
+        const int dx[] = {0, 0, -1, 1};
+        const int dy[] = {-1, 1, 0, 0};
+        
         init();
-        const int dx[] = {-1, 1, 0, 0};
-        const int dy[] = {0, 0, -1, 1};
-
-        bool haveZero = false;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (grid[i][j] == 0) {
-                    haveZero = true;
-                    continue;
-                }
-
-                for (int k = 0; k < 4; k++) {
-                    const int nextX = i + dx[k], nextY = j + dy[k];
-                    if (pointValid(nextX, nextY) && grid[nextX][nextY] == 1) {
-                        merge(ID(i, j), ID(nextX, nextY));
-                    }
+        int ans = 1;
+        for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) {
+            if (grid[i][j] == 0) continue;
+            
+            for (int k = 0; k < 4; k++) {
+                const int nx = i + dx[k], ny = j + dy[k];
+                if (point_valid(nx, ny) && grid[nx][ny] == 1) {
+                    merge(id(i, j), id(nx, ny));
+                    ans = max(ans, max(
+                        size[find_parent(id(i, j))],
+                        size[find_parent(id(nx, ny))]
+                    ));
                 }
             }
         }
-
-        unordered_set<int> visited;
-        multiset<int> componentSize;
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (grid[i][j] != 1) continue;
-
-                const int parent = findParent(ID(i, j));
-                if (!visited.count(parent)) {
-                    visited.insert(parent);
-                    componentSize.insert(size[parent]);
+        
+        for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) {
+            if (grid[i][j] == 1) continue;
+            
+            unordered_set<int> components;
+            for (int k = 0; k < 4; k++) {
+                const int nx = i + dx[k], ny = j + dy[k];
+                if (point_valid(nx, ny) && grid[nx][ny] == 1) {
+                    components.insert(find_parent(id(nx, ny)));
                 }
             }
+            
+            int cur = 1;
+            for (int i : components) cur += size[i];
+            ans = max(ans, cur);
         }
-
-        if (!haveZero) {
-            return *componentSize.rbegin();
+        return ans;
+    }
+private:
+    int n, m;
+    int parent[260001], size[260001];
+    
+    bool point_valid(int i, int j) {
+        return i >= 0 && j >= 0 && i < n && j < m;
+    }
+    
+    int id(int i, int j) {
+        return i * m + j;
+    }
+    
+    void init() {
+        for (int i = 0; i <= (n * m) + 1; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
-
-        if (componentSize.size() == 0) {
-            return 1;
-        } else if (componentSize.size() == 1) {
-            return *componentSize.rbegin() + 1;
+    }
+    
+    int find_parent(int u) {
+        if (parent[u] == u) return u;
+        else return parent[u] = find_parent(parent[u]);
+    }
+    
+    void merge(int u, int v) {
+        const int pu = find_parent(u), pv = find_parent(v);
+        if (parent[pu] != pv) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
         }
-
-        int answer = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                int temp = 0;
-                if (grid[i][j] == 1) continue;
-                unordered_set<int> visited;
-
-                for (int k = 0; k < 4; k++) {
-                    const int nextX = i + dx[k], nextY = j + dy[k];
-                    if (pointValid(nextX, nextY) && grid[nextX][nextY] == 1) {
-                        const int parent = findParent(ID(nextX, nextY));
-                        if (!visited.count(parent)) {
-                            visited.insert(parent);
-                            temp += size[parent];
-                        }
-                    }
-                }
-
-                answer = max(answer, temp + 1);
-            }
-        }
-        return answer;
     }
 };
