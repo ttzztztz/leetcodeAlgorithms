8aca721b2f2367f3a035d25336875979b4148a11
Rabbit <ttzztztz@outlook.com>
2021-08-04 19:56:14 +0800

    0804

--- 树/113. Path Sum II.cpp
+++ 树/113. Path Sum II.cpp
@@ -4,43 +4,38 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<vector<int>> pathSum(TreeNode* root, int sum) {
-        if (root == nullptr) return answer;
-        this->target = sum;
-        dfs(root, 0);
-        return answer;
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr) return ans;
+        this->target = targetSum;
+        
+        cur.push_back(root->val);
+        dfs(root, root->val);
+        return ans;
     }
 private:
-    vector<vector<int>> answer;
-    vector<int> current;
+    vector<vector<int>> ans;
+    vector<int> cur;
     int target;
-    void dfs(TreeNode* root, int cur) {
-        const bool isLeaf = !(root->left) && !(root->right);
+    
+    void dfs(TreeNode* u, int sum) {
+        if (u->left == nullptr && u->right == nullptr && sum == target) ans.push_back(cur);
         
-        if (isLeaf) {
-            if (cur + root->val == target) {
-                current.push_back(root->val);
-                answer.push_back(current);
-                current.pop_back();
-            }
-            return;
+        if (u->left) {
+            cur.push_back(u->left->val);
+            dfs(u->left, sum + u->left->val);
+            cur.pop_back();
         }
-        
-        if (root->left) {
-            current.push_back(root->val);
-            dfs(root->left, cur + root->val);
-            current.pop_back();
-        }
-        
-        if (root->right) {
-            current.push_back(root->val);
-            dfs(root->right, cur + root->val);
-            current.pop_back();
+        if (u->right) {
+            cur.push_back(u->right->val);
+            dfs(u->right, sum + u->right->val);
+            cur.pop_back();
         }
     }
 };
