37bc88e53c99cc8e88645d538e4b2ac8f7e0e12f
Rabbit <ttzztztz@outlook.com>
2021-08-07 15:36:45 +0800

    0807

--- 动态规划/132. Palindrome Partitioning II.cpp
+++ 动态规划/132. Palindrome Partitioning II.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
-    int N;
-    bool judge(const string& s, int start, int end) {
-        while (start < end) {
-            if (s[start] != s[end]) {
-                return false;
-            } else {
-                start++;
-                end--;
-            }
-        }
-        return true;
-    }
-    int dfs(vector<int>& dp, const string& s, int index) {
-        if (index == N) {
-            return 0;
-        }
-        if (dp[index] != -1) {
-            return dp[index];
-        }
-
-        int answer = 99999999;
-        for (int i = index; i < N; i++) {
-            if (judge(s, index, i)) {
-                answer = min(answer, 1 + dfs(dp, s, i + 1));
-            }
-        }
-        return dp[index] = answer;
-    }
     int minCut(string s) {
-        N = s.size();
-        vector<int> dp(N + 5, -1);
-
-        return dfs(dp, s, 0) - 1;
+        this->s = s;
+        n = s.size();
+        f = vector<vector<int>>(n, vector<int>(n, -1));
+        g = vector<int>(n, -1);
+        
+        return dfs(0) - 1;
+    }
+private:
+    vector<vector<int>> f;
+    vector<int> g;
+    string s;
+    int n;
+    
+    int is_palindrome(int i, int j) {
+        if (i > j) return false;
+        if (i == j) return true;
+        if (i + 1 == j) return s[i] == s[j];
+        
+        int& val = f[i][j];
+        if (val != -1) return val;
+        
+        int ans = 0;
+        if (s[i] == s[j]) ans = is_palindrome(i + 1, j - 1);
+        return val = ans;
+    }
+    
+    int dfs(int idx) {
+        if (idx == n) return 0;
+        int& val = g[idx];
+        if (val != -1) return val;
+        
+        int ans = n - idx;
+        for (int j = idx; j < n; j++) {
+            if (is_palindrome(idx, j)) ans = min(ans, 1 + dfs(j + 1));
+        }
+        return val = ans;
     }
 };
