9ce6f0040049d880b735dfef20028d2b5569c50a
Rabbit <ttzztztz@outlook.com>
2021-08-14 16:57:00 +0800

    0814

--- 动态规划/546. Remove Boxes.cpp
+++ 动态规划/546. Remove Boxes.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
-    int dfs(const vector<int>& boxes, int i, int j, int k) {
-        if (i == j) {
-            return (k + 1) * (k + 1);
-        }
-        if (i > j) {
-            return 0;
-        }
-        if (dp[i][j][k] != -1) {
-            return dp[i][j][k];
-        }
-
-        int answer = dfs(boxes, i, j - 1, 0) + (k + 1) * (k + 1);
-        for (int p = i; p + 1 <= j; p++) {
+    int removeBoxes(vector<int>& boxes) {
+        this->boxes = boxes;
+        n = boxes.size();
+        memset(f, 0xff, sizeof f);
+        
+        return dfs(0, n - 1, 0);
+    }
+private:
+    int n;
+    vector<int> boxes;
+    int f[101][101][101];
+    
+    int dfs(int i, int j, int k) {
+        if (i == j) return (k + 1) * (k + 1);
+        if (i > j) return 0;
+        int& val = f[i][j][k];
+        if (val != -1) return val;
+        
+        int ans = (k + 1) * (k + 1) + dfs(i, j - 1, 0);
+        for (int p = i; p < j; p++) {
             if (boxes[p] == boxes[j]) {
-                answer = max(answer, dfs(boxes, i, p, k + 1) + dfs(boxes, p + 1, j - 1, 0));
+                ans = max(ans, dfs(i, p, k + 1) + dfs(p + 1, j - 1, 0));
             }
         }
-        return dp[i][j][k] = answer;
-    }
-    int removeBoxes(vector<int>& boxes) {
-        const int N = boxes.size();
-        dp = vector<vector<vector<int>>>(N + 5, vector<vector<int>>(N + 5, vector<int>(N + 5, -1)));
-
-        return dfs(boxes, 0, N - 1, 0);
+        return val = ans;
     }
 };
